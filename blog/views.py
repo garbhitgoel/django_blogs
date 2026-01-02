ca2b6d25b759f786d7d804be386f58b56dcdfe1f
garbhitgoel <garbhit.goel@gmail.com>
2026-01-02 23:49:19 +0530

    Day 2 -- Working on Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {   
+        'author': 'Garbhit Goel',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 01, 2026'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 02, 2026'
+    }
+]
 
 def home(request):
-    return HttpResponse("<h1>Welcome to the Blog Home Page!</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
 
 # Create your views here.
